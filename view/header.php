0a2264a6bba4559f14e428af9877474bf3d7f189
pranish07 <pranishshrestha181@gmail.com>
2024-06-03 21:25:17 +0545

    style:made style changes

--- view/header.php
+++ view/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment Tracker</title>
-    <link rel="stylesheet" href="view/css/main.min.css" />
+    <link rel="stylesheet" href="view/css/main.css" />
 </head>
 
 <body>
